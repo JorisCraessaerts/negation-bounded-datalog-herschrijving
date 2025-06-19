7221e00667a6115b601c10744302083d06e2285d
Joris Craessaerts <craessaerts.joris@outlook.com>
2025-06-19 11:54:42 +0200

    Nog wat nutteloze variabelen uit T1 en T2 gehaald

--- input_generator.py
+++ input_generator.py
@@ -20,27 +20,9 @@ import os
 #         f.write(f"{a}\t{b}\n")
 
 
-NUM_NODES = 100
-MAX_EDGES = NUM_NODES * (NUM_NODES - 1)
-NUM_EDGES = min(2000, MAX_EDGES)
-
-edges = set()
-
-while len(edges) < NUM_EDGES:
-    a = random.randint(1, NUM_NODES)
-    b = random.randint(1, NUM_NODES)
-    if a != b:
-        edges.add((a, b))
-
-os.makedirs("facts", exist_ok=True)
-
-with open("facts/edge.facts", "w") as f:
-    for a, b in edges:
-        f.write(f"{a}\t{b}\n")
-
-# NUM_NODES = 1000
+# NUM_NODES = 100
 # MAX_EDGES = NUM_NODES * (NUM_NODES - 1)
-# NUM_EDGES = min(100000, MAX_EDGES)
+# NUM_EDGES = min(2000, MAX_EDGES)
 
 # edges = set()
 
@@ -55,6 +37,24 @@ with open("facts/edge.facts", "w") as f:
 # with open("facts/edge.facts", "w") as f:
 #     for a, b in edges:
 #         f.write(f"{a}\t{b}\n")
+
+NUM_NODES = 1000
+MAX_EDGES = NUM_NODES * (NUM_NODES - 1)
+NUM_EDGES = min(100000, MAX_EDGES)
+
+edges = set()
+
+while len(edges) < NUM_EDGES:
+    a = random.randint(1, NUM_NODES)
+    b = random.randint(1, NUM_NODES)
+    if a != b:
+        edges.add((a, b))
+
+os.makedirs("facts", exist_ok=True)
+
+with open("facts/edge.facts", "w") as f:
+    for a, b in edges:
+        f.write(f"{a}\t{b}\n")
 
 # NUM_NODES = 10000
 # MAX_EDGES = NUM_NODES * (NUM_NODES - 1)
